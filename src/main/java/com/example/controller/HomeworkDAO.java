3edb60f674819cc1df19b5e5ad12a1add0108e98
Adam Dicky <admdcky@gmail.com>
2025-01-06 23:41:01 +0800

    Figuring out homework databaes

--- src/main/java/com/example/controller/HomeworkDAO.java
+++ src/main/java/com/example/controller/HomeworkDAO.java
@@ -1,5 +1,29 @@
 package com.example.controller;
 
-public class HomeworkDAO {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class HomeworkDAO {
+	
+	 private String jdbcURL = "jdbc:mysql://localhost:3306/user_db"; // Update DB name if needed
+	 private String jdbcUsername = "root";
+	 private String jdbcPassword = "";
+	 
+	 static {
+		 // Load MySQL JDBC driver
+		 try {
+			 Class.forName("com.mysql.jdbc.Driver");
+			 System.out.println("MySQL JDBC Driver successfully loaded.");
+	     } 
+		 
+		 catch (ClassNotFoundException e) {
+	            System.out.println("MySQL JDBC Driver not found.");
+	            e.printStackTrace();
+		 }
+	 }
+	 
+	 private Connection getConnection() throws SQLException {
+	        return DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+	 }
 }
